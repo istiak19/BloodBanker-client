e8f02c2e920485367465e52d41fd24a7c4dcbd48
istiak19 <istiak15-14128@diu.edu.bd>
2025-01-15 07:30:23 +0600

    login page setup

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,60 @@
+import Lottie from 'lottie-react';
+import loginPic from '../../assets/lottie/design.json'
+import { useForm } from "react-hook-form";
+import useAuth from '../../Hook/useAuth';
+
+const Login = () => {
+    const { signin } = useAuth();
+
+
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+        signin(data.email, data.password)
+            .then((result) => {
+                console.log(result.user)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    };
+
+    return (
+        <div className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left ml-10">
+                    <Lottie className='w-[500px]' animationData={loginPic}></Lottie>
+                </div>
+                <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl">
+                    <h1 className="text-5xl font-bold pt-3 text-center">Login now!</h1>
+                    <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" name='email'
+                                {...register("email")} className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password"
+                                name='password'
+                                {...register("password")}
+                                placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn bg-red-400 hover:bg-red-500 text-white">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Register from "../pages/Register/Register";
 import MainLayout from "../layout/MainLayout";
+import Login from "../pages/Login/Login";
 
 const routers = createBrowserRouter([
     {
         path: "/",
         element: <MainLayout></MainLayout>,
         children: [
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
             {
                 path: "/register",
                 element: <Register></Register>
